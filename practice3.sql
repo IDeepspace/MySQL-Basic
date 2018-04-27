64518136a9d74bd2a65f06d678def1e811f17533
IDeepspace <cxin1427@qq.com>
2018-04-27 10:56:55 +0800

    完成practice3

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,16 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
+# 先找出所有公司的公司名和平均薪水
+select companyName, avg(salary) as avgSalary
+from Company left join Employee
+on Company.id = Employee.companyId
+group by companyId;
+
+# 找出平均薪水最高的
+select companyName, max(avgSalary) as avgSalary from
+(select companyName, avg(salary) as avgSalary
+from Company left join Employee
+on Company.id = Employee.companyId
+group by companyId) as t;
 
